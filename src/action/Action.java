d0815023897a4995e216476384cd9c3ba37b1fc5
Luxin Zhang <zhangluxin0317@gmail.com>
2016-06-09 17:12:19 +0200

    all manipulation fonctions

--- src/action/Action.java
+++ src/action/Action.java
@@ -12,49 +12,168 @@ public class Action {
 	
 	public void newAnnonce(String categorie, String nom, String rue, String ville,
 		String postal, String tele, String text) {
+
+		if(checkCategorie(categorie) && checkPostTel(postal, tele)) {
+			DBConnecter db = new DBConnecter();
+			Connection con = db.getConnection();
+			String sql = "insert into annonce(categorie, nom, rue, ville, code_postale, telephone, text) "
+					+ "values(?, ?, ?, ?, ?, ?, ?)";
+			try {
+				PreparedStatement prest = con.prepareStatement(sql);
+				prest.setString(1, categorie);
+				prest.setString(2, nom);
+				prest.setString(3, rue);
+				prest.setString(4, ville);
+				prest.setString(5, postal);
+				prest.setString(6, tele);
+				prest.setString(7, text);
+				prest.executeUpdate();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			db.close();	
+		}
+	}
+	
+	public void modifyAnnonce(String categorie, String nom, String rue, String ville,
+		String postal, String tele, String text, int id_annonce){
+
+		if(checkCategorie(categorie) && checkAnnonce(id_annonce)
+				&& checkPostTel(postal, tele)) {
+			DBConnecter db = new DBConnecter();
+			Connection con = db.getConnection();
+			String sql = "update annonce set categorie=?, nom=?, rue=?, ville=?, code_postale=?, telephone=?, text=? "
+					+ "where id_annonce=?";
+			try {
+				PreparedStatement prest = con.prepareStatement(sql);
+				prest.setString(1, categorie);
+				prest.setString(2, nom);
+				prest.setString(3, rue);
+				prest.setString(4, ville);
+				prest.setString(5, postal);
+				prest.setString(6, tele);
+				prest.setString(7, text);
+				prest.setInt(8, id_annonce);
+				prest.executeUpdate();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			db.close();	
+		}
+	}
+	
+	public void deleteAnnonce(int id_annonce) {
+		if(checkAnnonce(id_annonce)) {
+			DBConnecter db = new DBConnecter();
+			Connection con = db.getConnection();
+			String sql = "delete from annonce where id_annonce = ?";
+			try {
+				PreparedStatement prest = con.prepareStatement(sql);
+				prest.setInt(1, id_annonce);
+				prest.executeUpdate();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			db.close();	
+		}
+	}
+	
+	public void newCategorie(String categorie) {
 		DBConnecter db = new DBConnecter();
 		Connection con = db.getConnection();
-		String sql = "insert into annonce(categorie, nom, rue, ville, code_postale, telephone, text) "
-				+ "values(?, ?, ?, ?, ?, ?, ?)";
 		try {
-			PreparedStatement prest = con.prepareStatement(sql);
-			prest.setString(1, categorie);
-			prest.setString(2, nom);
-			prest.setString(3, rue);
-			prest.setString(4, ville);
-			prest.setString(5, postal);
-			prest.setString(6, tele);
-			prest.setString(7, text);
-			prest.executeUpdate();
+			if(!checkCategorie(categorie)) {
+				String sql = "insert into categorie(title) values(?)";
+				PreparedStatement prest = con.prepareStatement(sql);
+				prest.setString(1, categorie);
+				prest.executeUpdate();	
+			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		db.close();
 	}
+
+	public void deleteCategorie(String categorie) {
+		if(checkCategorie(categorie) && !hasAnnonce(categorie)) {
+			DBConnecter db = new DBConnecter();
+			Connection con = db.getConnection();
+			try {
+				String sql = "delete from categorie where title=?";
+				PreparedStatement prest = con.prepareStatement(sql);
+				prest.setString(1, categorie);
+				prest.executeUpdate();	
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			db.close();	
+		}
+	}
+
+	private Boolean checkPostTel(String codePostal, String tel){
+		return true;
+	}
 	
-	public void modifyAnnonce(String categorie, String nom, String rue, String ville,
-		String postal, String tele, String text, int id_annonce){
+	private Boolean hasAnnonce(String categorie) {
 		DBConnecter db = new DBConnecter();
-		Connection con = db.getConnection();
-		String sql = "update annonce set categorie=?, nom=?, rue=?, ville=?, code_postale=?, telephone=?, text=? "
-				+ "where id_annonce=?";
+		Statement st = db.getStatement();
 		try {
-			PreparedStatement prest = con.prepareStatement(sql);
-			prest.setString(1, categorie);
-			prest.setString(2, nom);
-			prest.setString(3, rue);
-			prest.setString(4, ville);
-			prest.setString(5, postal);
-			prest.setString(6, tele);
-			prest.setString(7, text);
-			prest.setInt(8, id_annonce);
-			prest.executeUpdate();
+			ResultSet rs = st.executeQuery("select * from categorie inner join annonce "
+					+ "on title = categorie where categorie =" + categorie);			
+			if(rs.next()) {
+				return true;
+			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		db.close();
+		return false;
+	}
+	
+	private Boolean checkCategorie(String categorie){
+		DBConnecter db = new DBConnecter();
+		Statement st = db.getStatement();
+		try {
+			ResultSet rs = st.executeQuery("select * from categorie");
+			while(rs.next()) {
+				if(rs.getString("title").equals(categorie)) {
+					rs.close();
+					db.close();
+					return true;
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		db.close();
+		return false;
+	}
+	
+	private Boolean checkAnnonce(int id_annonce){
+		DBConnecter db = new DBConnecter();
+		Statement st = db.getStatement();
+		try {
+			ResultSet rs = st.executeQuery("select * from annonce");
+			while(rs.next()) {
+				if(rs.getInt("id_annonce") == id_annonce) {
+					rs.close();
+					db.close();
+					return true;
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		db.close();
+		return false;
 	}
 	
 	public String test(){
